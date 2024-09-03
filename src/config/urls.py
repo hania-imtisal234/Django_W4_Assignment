890c51b3e1a315ab5792b166092b112ddad6e400
sadiqAjmal14 <muhammad.sadiq@techverx.com>
2024-09-04 04:12:01 +0500

    important changes

--- src/config/urls.py
+++ src/config/urls.py
@@ -1,27 +1,10 @@
-"""
-URL configuration for config project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-# config/urls.py
-
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls import handler403, handler400, handler500, handler404
 from django.shortcuts import render
+from web.users.views import homepage_view
 
 def custom_permission_denied_view(request, exception=None):
     return render(request, 'errors/403.html', status=403)
@@ -32,20 +15,20 @@ def custom_bad_request_view(request, exception=None):
 def custom_error_view(request):
     return render(request, 'errors/500.html', status=500)
 
-def custom_404_view(request,exception):
-    return render(request, 'errors/500.html', status=404)
+def custom_404_view(request, exception):
+    return render(request, 'errors/404.html', status=404)
 
 handler403 = custom_permission_denied_view
 handler400 = custom_bad_request_view
 handler500 = custom_error_view
-handler404=custom_404_view
-
+handler404 = custom_404_view
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('web.appointments.urls')),
-    path('', include('web.medical_records.urls')),  
-    path('', include('web.users.urls')),
+    path('admin/', admin.site.urls, name='admin_site'),
+    path('appointments/', include('web.appointments.urls')),
+    path('medical_records/', include('web.medical_records.urls')),
+    path('users/', include('web.users.urls')),
+    path('', homepage_view, name='index'),
 ]
 
 if settings.DEBUG:
